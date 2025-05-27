a9f8c776b226b6e9eeface9d473e0d782ecad793
unknown <tkiely99@gmail.com>
2025-05-26 22:38:24 -0400

    background

---
+++ src/components/GradientBackground.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import './GradientBackground.css';
+
+const GradientBackground: React.FC = () => {
+  return <div className="gradient-background" />;
+};
+
+export default GradientBackground;
