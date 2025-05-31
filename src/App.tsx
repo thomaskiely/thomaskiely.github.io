0714b3899ee0fbf001782fcbe60678d42dc78f88
unknown <tkiely99@gmail.com>
2025-05-31 19:42:20 -0400

    add beam effect

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
-import GradientBackground from './components/GradientBackground';
-
+import { BackgroundBeams } from './components/Beams/Beams';
+import "./App.css";
 function App() {
   return (
-    <>
-      <GradientBackground />
-      <div className="content">
-      </div>
-    </>
+    <div className="relative h-screen w-screen bg-black overflow-hidden">
+      <BackgroundBeams className="opacity-50" />
+      <main className="relative z-10 flex items-center justify-center h-full text-white">
+        <h1 className="text-4xl font-bold">Welcome to my app!</h1>
+      </main>
+    </div>
   );
 }
 
