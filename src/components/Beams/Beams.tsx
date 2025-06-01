23e3d3b1cc000f9b5e61c31a3540b73515ae69d8
unknown <tkiely99@gmail.com>
2025-05-31 20:03:41 -0400

    fix beams

--- src/components/Beams/Beams.tsx
+++ src/components/Beams/Beams.tsx
@@ -89,7 +89,7 @@ export const BackgroundBeams = React.memo(
             ></motion.path>
           ))}
           <defs>
-            {paths.map((index) => (
+            {paths.map((_, index) => (
               <motion.linearGradient
                 id={`linearGradient-${index}`}
                 key={`gradient-${index}`}
